9597cce60a73afe4c0c5ba53b69b1ba8ab40ce34
KhayyamAhmadov <xeyyam.ehmedov30@gmail.com>
2025-05-22 23:29:11 +0400

    BusinessTrip.py

--- BusinessTrip.py
+++ BusinessTrip.py
@@ -9,7 +9,7 @@ st.title("✈️ Ezamiyyət hesablayıcı - Giriş")
 password = st.text_input("Sayta giriş üçün şifrəni daxil edin:", type="password")
 
 # Daxil ediləcək şifrə
-correct_password = "secret123"
+correct_password = "admin"
 
 if password != correct_password:
     st.warning("Zəhmət olmasa düzgün şifrəni daxil edin.")
